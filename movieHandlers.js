7c82c5065ba526a7b440b91ce5c53dc438a6ff8f
vladka-kl <84926603+vladka-kl@users.noreply.github.com>
2022-12-12 14:10:43 +0100

    without challenge

--- movieHandlers.js
+++ movieHandlers.js
@@ -25,8 +25,24 @@ const movies = [
   },
 ];
 
+const database = require("./database");
+
 const getMovies = (req, res) => {
-  res.json(movies);
+  const id = parseInt(req.params.id);
+  database
+    .query("select * from movies where id = ?", [id])
+    .then(([movies]) => {
+      if (movies[0] != null) {
+        res.json();
+        res.json(movies[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 const getMovieById = (req, res) => {
